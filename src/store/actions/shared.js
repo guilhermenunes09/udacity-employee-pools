129347bbab5fc6989639b17a42abf93c28cb8b07
Guilherme Nunes <guilhermewnunes@gmail.com>
2022-11-26 19:40:15 -0300

    type: feature
    Get users from the API and call it throught the handleInitialData in App.js

--- src/store/actions/shared.js
+++ src/store/actions/shared.js
@@ -1,10 +1,12 @@
 import { getInitialData } from "../../utils/api";
+import { receiveUsers } from "./users";
 import { receiveQuestions } from "./questions";
 
 export function handleInitialData () {
   return (dispatch) => {
 
-    return getInitialData().then(({questions}) => {
+    return getInitialData().then(({users, questions}) => {
+      dispatch(receiveUsers(users))
       dispatch(receiveQuestions(questions));
     })
   }
